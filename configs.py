be5e20feec07ac82ddf2fcd48173a182084c3145
Sawansingh24 <119250143+Sawansingh24@users.noreply.github.com>
2022-12-24 22:18:06 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
-# (c) @RoyalKrrishna
+# (c) @Sawan_singh_24
 
 import os
 # from dotenv import load_dotenv
@@ -9,11 +9,11 @@ import os
 class Config(object):
     API_ID = int(os.getenv("API_ID", "16445683"))
     API_HASH = os.getenv("API_HASH", "d0852e13eee2389ff2d9183b00649547")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "5690447308:AAFlxJhhGZyzME8OeQzJj4VgxYFfDGiQQuc")
-    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "CwRobot")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "5864047341:AAGKF_cQKxLPnJNlW0xJHE3iBH-GOyywKPk")
+    BOT_SESSION_NAME = os.getenv("BOT_SESSION_NAME", "Cw_robot_bot")
     USER_SESSION_STRING = os.getenv("USER_SESSION_STRING", "1BVtsOGcBu6bYmjNgvA8CwAlw3rsFwazr7QkoX3229UrN1YixC2zhVdy_3UCWo722eLX2SRyAziHfn-aK419-7n6HMpK47lqyQWh-XbhiYWb9cOkkXHvPg6aPEiOLrYxQRDrAQZzy-tWJlnso2qnffUNEw3DBQjCVxo9PhFiUbsKvV-JoCj2sISnDuckWikctX9meY2Xn6ODFnm1LfxA_-N8Y27yM_YO_NwlI6O5DthUC8TRGxQKUsU1S6Zm-1p6zSwI4UieslALHGnnn6WGh8BK1bGsO_HHsuk0AOZgcbuZai4bEo0-_reORADH7fQHNOosn0sga3v6t0GJapDOZMP2onNAqAJI=")
     CHANNEL_ID = int(os.getenv("CHANNEL_ID", -1001761239456))
-    BOT_USERNAME = os.getenv("CW_Robot_bot")
+    BOT_USERNAME = os.getenv("Cw_robot_bot")
     BOT_OWNER = int(os.getenv("2091430700"))
 #    OWNER_USERNAME = os.getenv("@Sawan_singh_24")
     BACKUP_CHANNEL = os.getenv("CW_wins_movie")
@@ -32,7 +32,7 @@ Fᴏʀ Mᴏʀᴇ Iɴꜰᴏ Cʟɪᴄᴋ Oɴ Hᴇʟᴘ ✅''')
 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏ ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ✅''')
     UPDATES_CHANNEL = os.getenv("CW_wins_movie", None)
     DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://Sawansingh24:Sawansingh24@cluster0.uiuhxxj.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1001756502720"))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "--1001871848830"))
     RESULTS_COUNT = int(os.getenv("RESULTS_COUNT", 5))
     BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True")
     UPDATES_CHANNEL_USERNAME = os.getenv("Update_channel_name", "@CW_wins_movie")
